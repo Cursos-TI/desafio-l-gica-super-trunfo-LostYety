fcb807ea33ddd32279b02da5487ef3607b06b964
LostYety <olvgbb31@gmail.com>
2025-04-21 13:43:51 +0000

    Código Pronto

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
 
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+
 int main() {
     // Atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     char cod_cidade[4], cod_cidade2[4];
     char nome_cidade[50], nome_cidade2[50];
-    char cidadeVencedora[] = "Cidade vencedora";
+    char cidadeVencedora[20];
     int populacao, populacao2;
     float area, area2;
     float pib, pib2;
@@ -87,22 +88,19 @@ int main() {
     printf("Nome da cidade 2: %s\n", nome_cidade2);
     printf("PIB 2: R$ %.2f\n", pib2);
     printf("Tamanho da população 2: %d\n", populacao2);
-    printf("Tamanho da área 2: %.2fkm²\n²", area2);
-    printf("Quantidade de Pontos turísticos 2: %d\n", ponto_turistico2);
+    printf("Tamanho da área 2: %.2fkm²\n", area2);
+    printf("Quantidade de Pontos turísticos 2: %d\n\n", ponto_turistico2);
    
+    //Lógica
     if (populacao > populacao2) {
-        strcpy(cidadeVencedora, "")
+        strcpy(cidadeVencedora, "Cidade 1");
         printf("Cidade 1 tem maior população.\n");
     } else {
-        cidadeVencedora = populacao2;
+        strcpy(cidadeVencedora, "Cidade 2");
         printf("Cidade 2 tem maior população.\n");
      }
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    
+     
     printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
     return 0;
